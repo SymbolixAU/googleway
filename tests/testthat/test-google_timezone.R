97aff827c4e02d68ae429b0a79ea92763387e10a
SymbolixAU <dcooley@symbolix.com.au>
2016-06-05 17:09:45 +1000

    timezone tests

---
+++ tests/testthat/test-google_timezone.R
@@ -0,0 +1,52 @@
+context("Google timezone")
+
+
+test_that("location is a pair of coordinates",{
+
+  expect_error(google_timezone(location = c(144.9841),
+                               timestamp = as.POSIXct("2016-06-05"),
+                               output_format = "data.frame",
+                               key = "abc"),
+               "location must be a vector of a pair of latitude and longitude coordinates")
+
+  expect_error(google_timezone(location = "location",
+                               timestamp = as.POSIXct("2016-06-05"),
+                               output_format = "data.frame",
+                               key = "abc"),
+               "location must be a vector of a pair of latitude and longitude coordinates")
+
+})
+
+
+test_that("timestamp is POSIXct",{
+
+  expect_error(google_timezone(location = c(-37.81659, 144.9841),
+                               timestamp = "2016-06-05",
+                               output_format = "data.frame",
+                               key = "abc"),
+               "timestamp must be a POSIXct object")
+
+})
+
+
+test_that("language is a single string",{
+
+  expect_error(google_timezone(location = c(-37.81659, 144.9841),
+                               timestamp = as.POSIXct("2016-06-05"),
+                               output_format = "data.frame",
+                               language = c("en", "fr"),
+                               key = "abc"),
+                "language must be a single character vector or string")
+
+  expect_error(google_timezone(location = c(-37.81659, 144.9841),
+                               timestamp = as.POSIXct("2016-06-05"),
+                               output_format = "data.frame",
+                               language = 1,
+                               key = "abc"),
+               "language must be a single character vector or string")
+
+})
+
+
+
+
