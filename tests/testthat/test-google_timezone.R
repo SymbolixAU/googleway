4ce678a82041fc87a677efd969f48eed82ac917c
SymbolixAU <dcooley@symbolix.com.au>
2016-06-26 13:33:02 +1000

    timezone test - skip on CRAN

--- tests/testthat/test-google_timezone.R
+++ tests/testthat/test-google_timezone.R
@@ -1,6 +1,5 @@
 context("Google timezone")
 
-
 test_that("location is a pair of coordinates",{
 
   expect_error(google_timezone(location = c(144.9841),
@@ -46,6 +45,29 @@ test_that("language is a single string",{
                "language must be a single character vector or string")
 
 })
+
+test_that("simplify if logical", {
+
+  expect_error(google_timezone(location = c(-37.81659, 144.9841),
+                               timestamp = as.POSIXct("2016-06-05"),
+                               simplify = "TRUE",
+                               key = "abc"),
+               "simplify must be logical - TRUE or FALSE")
+
+})
+
+test_that("Timezone Australia/Hobart is returned",{
+
+  skip_on_cran()
+  key <- read.dcf("./.googleAPI", fields = c("GOOGLE_API_KEY"))
+
+  expect_equal(google_timezone(location = c(-37.81659, 144.9841),
+                               timestamp = as.POSIXct("2016-06-05"),
+                               simplify = TRUE,
+                               key = key)$timeZoneId,
+               "Australia/Hobart")
+
+  })
 
 
 
