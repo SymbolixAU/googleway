02c68b7f88ff2ab32d0199b5f8a82b78e457d575
SymbolixAU <dcooley@symbolix.com.au>
2016-07-06 19:05:59 +1000

    playing with map widget

--- R/google_map.R
+++ R/google_map.R
@@ -1,32 +1,39 @@
 
-library(shiny)
-library(shinydashboard)
+# library(shiny)
+# library(shinydashboard)
+#
+# key <- read.dcf("~/Documents/.googleAPI", fields = c("GOOGLE_API_KEY"))
+#
+# ui <- dashboardPage(
+#   dashboardHeader(),
+#   dashboardSidebar(),
+#   dashboardBody(
+#
+#     uiOutput("map")
+#
+#   )
+# )
+#
+# server <- function(input, output){
+#
+#   output$map <- renderUI({
+#
+#     ## static map
+#     tags$iframe(src = paste0("https://www.google.com/maps/embed/v1/place?q=MCG,Melbourne%20Rd,%20AUS
+#         &zoom=14
+#         &key=", key) )
+#
+#   })
+# }
+#
+# shinyApp(ui, server)
+#
 
-key <- read.dcf("~/Documents/.googleAPI", fields = c("GOOGLE_API_KEY"))
+library(htmlwidgets)
+library(devtools)
 
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody(
+key <- read.dcf("~/Documents/.googleAPI", fields = c("SYMBOLIX_GOOGLE_API_KEY"))
 
-    uiOutput("map")
-
-  )
-)
-
-server <- function(input, output){
-
-  output$map <- renderUI({
-
-    ## static map
-    tags$iframe(src = paste0("https://www.google.com/maps/embed/v1/place?q=MCG,Melbourne%20Rd,%20AUS
-        &zoom=14
-        &key=", key) )
-
-  })
-}
-
-shinyApp(ui, server)
 
 
 
