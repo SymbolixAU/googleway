a3cdde3178c097c9fc5516f4b8492be782dbe413
SymbolixAU <dcooley@symbolix.com.au>
2016-11-24 19:47:14 +1100

    udated news and more tests

--- tests/testthat/test-google_map_layers.R
+++ tests/testthat/test-google_map_layers.R
@@ -1,7 +1,6 @@
 context("google map layers")
 
 
-
 test_that("markers correctly defined", {
 
   m <- google_map(key = "abc")
@@ -23,7 +22,21 @@ test_that("markers correctly defined", {
 
   expect_error(add_markers(map = m, data = df, cluster = 'yes'), "cluster must be logical")
 
+  df <- data.frame(lat = 1:4,
+                   lon = 1:4,
+                   title = letters[1:4],
+                   draggable = TRUE,
+                   opacity = 0.1,
+                   label = letters[1:4],
+                   info_window = letters[1:4],
+                   mouse_over = letters[1:4])
 
+  expect_true("title" %in% names(jsonlite::fromJSON(add_markers(map = m, data = df, title = "title")$x$calls[[1]]$args[[1]])))
+  expect_true("draggable" %in% names(jsonlite::fromJSON(add_markers(map = m, data = df, draggable = "draggable")$x$calls[[1]]$args[[1]])))
+  expect_true("opacity" %in% names(jsonlite::fromJSON(add_markers(map = m, data = df, opacity = "opacity")$x$calls[[1]]$args[[1]])))
+  expect_true("label" %in% names(jsonlite::fromJSON(add_markers(map = m, data = df, label = "label")$x$calls[[1]]$args[[1]])))
+  expect_true("info_window" %in% names(jsonlite::fromJSON(add_markers(map = m, data = df, info_window = "info_window")$x$calls[[1]]$args[[1]])))
+  expect_true("mouse_over" %in% names(jsonlite::fromJSON(add_markers(map = m, data = df, mouse_over = "mouse_over")$x$calls[[1]]$args[[1]])))
 
 })
 
