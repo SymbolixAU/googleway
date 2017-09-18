acf74640976f4c387a8f1ce14db12594cc8947ea
David Cooley <dcooley@symbolix.com.au>
2017-09-19 08:23:58 +1000

    tidies JS

--- inst/htmlwidgets/lib/polylines.js
+++ inst/htmlwidgets/lib/polylines.js
@@ -9,67 +9,65 @@
  * @param use_polyline
  *          boolean indicating if the data is an encoded polyline
  */
-function add_polylines(map_id, data_polyline, update_map_view, layer_id, use_polyline){
+function add_polylines(map_id, data_polyline, update_map_view, layer_id, use_polyline) {
 
-  window[map_id + 'googlePolyline' + layer_id] = [];
-  var infoWindow = new google.maps.InfoWindow();
+    window[map_id + 'googlePolyline' + layer_id] = [];
+    var infoWindow = new google.maps.InfoWindow();
+    
+    for (i = 0; i < Object.keys(data_polyline).length; i++) {
 
-  // if we are using a polyline column, then it's just one row per line
-  // if it's columns of lat/lon, then it's many rows per line
-  for(i = 0; i < Object.keys(data_polyline).length; i++){
+        if (use_polyline) {
+            thisPath = google.maps.geometry.encoding.decodePath(data_polyline[i].polyline);
+        } else {
+            thisPath = [];
 
-    if(use_polyline){
-      thisPath = google.maps.geometry.encoding.decodePath(data_polyline[i].polyline);
-    }else{
-      thisPath = [];
-
-      for(j = 0; j < Object.keys(data_polyline[i].coords).length; j++){
-        thisPath.push(data_polyline[i].coords[j]);
-      }
-    }
-    add_lines(map_id, data_polyline[i], layer_id, thisPath);
+            for (j = 0; j < Object.keys(data_polyline[i].coords).length; j++) {
+                thisPath.push(data_polyline[i].coords[j]);
+            }
+        }
+        add_lines(map_id, data_polyline[i], layer_id, thisPath);
   }
 
 
-  function add_lines(map_id, polyline, layer_id, thisPath){
+    function add_lines(map_id, polyline, layer_id, thisPath){
 
-    var Polyline = new google.maps.Polyline({
-      path: thisPath,
-      id: polyline.id,
-      geodesic: polyline.geodesic,
-      strokeColor: polyline.stroke_colour,
-      strokeOpacity: polyline.stroke_opacity,
-      strokeOpacityHolder: polyline.stroke_opacity,
-      strokeWeight: polyline.stroke_weight,
-      mouseOver: polyline.mouse_over,
-      mouseOverGroup: polyline.mouse_over_group,
-      zIndex: polyline.z_index
+        var Polyline = new google.maps.Polyline({
+            path: thisPath,
+            id: polyline.id,
+            geodesic: polyline.geodesic,
+            strokeColor: polyline.stroke_colour,
+            strokeOpacity: polyline.stroke_opacity,
+            strokeOpacityHolder: polyline.stroke_opacity,
+            strokeWeight: polyline.stroke_weight,
+            mouseOver: polyline.mouse_over,
+            mouseOverGroup: polyline.mouse_over_group,
+            zIndex: polyline.z_index
 
     });
 
-    // calculate and log the distance
-    polyLengthInMeters = google.maps.geometry.spherical.computeLength(Polyline.getPath().getArray());
-    console.log(polyLengthInMeters);
+    // TODO(polyline length) calculate and log the distance
+//    polyLengthInMeters = google.maps.geometry.spherical.computeLength(Polyline.getPath().getArray());
+//    console.log(polyLengthInMeters);
 
-    window[map_id + 'googlePolyline' + layer_id].push(Polyline);
-    Polyline.setMap(window[map_id + 'map']);
+        window[map_id + 'googlePolyline' + layer_id].push(Polyline);
+        Polyline.setMap(window[map_id + 'map']);
 
-    if(update_map_view === true){
-      // extend the bounds of the map
-      var points = Polyline.getPath().getArray();
+        if (update_map_view === true) {
+            // extend the bounds of the map
+            var points = Polyline.getPath().getArray();
 
-      for ( var n = 0; n < points.length; n++){
-        window[map_id + 'mapBounds'].extend(points[n]);
-      }
+            for (var n = 0; n < points.length; n++){
+                window[map_id + 'mapBounds'].extend(points[n]);
+            }
     }
 
-    if(polyline.info_window){
-      add_infoWindow(map_id, Polyline, infoWindow, '_information', polyline.info_window);
+    if(polyline.info_window) {
+        add_infoWindow(map_id, Polyline, infoWindow, '_information', polyline.info_window);
     }
 
     // need to add the listener once so it's not overwritten?
-    if(polyline.mouse_over || polyline.mouse_over_group){
-      add_mouseOver(map_id, Polyline, infoWindow, "_mouse_over", polyline.mouse_over, layer_id, 'googlePolyline');
+    if(polyline.mouse_over || polyline.mouse_over_group) {
+        add_mouseOver(map_id, Polyline, infoWindow, "_mouse_over", polyline.mouse_over, layer_id, 'googlePolyline');
     }
 
     polylineInfo = { layerId : layer_id };
@@ -104,8 +102,6 @@ function update_polylines(map_id, data_polyline, layer_id) {
         currentIds = [],
         newIds = [],
         newPolylines = [];
-    
-    console.log(data_polyline);
 
     if(window[map_id + 'googlePolyline' + layer_id] !== undefined ){
 
@@ -117,8 +113,6 @@ function update_polylines(map_id, data_polyline, layer_id) {
 
             // find if there is a matching id in the new polyline data set
             thisUpdatePolyline = findById(data_polyline, _id);
-            
-            console.log(thisUpdatePolyline);
             
             if(thisUpdatePolyline !== undefined){
                 //if(data_polyline.find(x => x.id === _id)){
@@ -170,10 +164,10 @@ function update_polylines(map_id, data_polyline, layer_id) {
 
 
 
-function clear_polylines(map_id, layer_id){
+function clear_polylines(map_id, layer_id) {
 
-  for (i = 0; i < window[map_id + 'googlePolyline' + layer_id].length; i++){
-    window[map_id + 'googlePolyline' + layer_id][i].setMap(null);
-  }
-  window[map_id + 'googlePolyline' + layer_id] = null;
+    for (i = 0; i < window[map_id + 'googlePolyline' + layer_id].length; i++){
+        window[map_id + 'googlePolyline' + layer_id][i].setMap(null);
+    }
+    window[map_id + 'googlePolyline' + layer_id] = null;
 }
