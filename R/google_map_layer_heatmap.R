28ca9125ea0e286aaa033a92734aa42d109054d1
David Cooley <dcooley@symbolix.com.au>
2017-09-18 14:05:13 +1000

    update heatmap

--- R/google_map_layer_heatmap.R
+++ R/google_map_layer_heatmap.R
@@ -138,3 +138,62 @@ add_heatmap <- function(map,
 
   invoke_method(map, 'add_heatmap', shape, heatmap_options, update_map_view, layer_id)
 }
+
+
+
+#' update heatmap
+#'
+#' updates a heatmap layer
+#'
+#' @param map a googleway map object created from \code{google_map()}
+#' @param data data frame containing at least two columns, one specifying the
+#' latitude coordinates, and the other specifying the longitude. If Null,
+#' the data passed into \code{google_map()} will be used.
+#' @param lat string specifying the column of \code{data} containing the 'latitude'
+#' coordinates. If left NULL, a best-guess will be made
+#' @param lon string specifying the column of \code{data} containing the 'longitude'
+#' coordinates. If left NULL, a best-guess will be made
+#' @param weight string specifying the column of \code{data} containing the 'weight'
+#' associated with each point. If NULL, each point will get a weight of 1.
+#' @param layer_id single value specifying an id for the layer.
+#' @param digits integer. Use this parameter to specify how many digits (decimal places)
+#' should be used for the latitude / longitude coordinates.
+#'
+#' @export
+update_heatmap <- function(map,
+                           data,
+                           lat = NULL,
+                           lon = NULL,
+                           weight = NULL,
+                           layer_id = NULL,
+                           digits = 4){
+
+  ## TODO: update_map_view options
+
+  objArgs <- match.call(expand.dots = F)
+  dataCheck(data)
+  layer_id <- layerId(layer_id)
+  objArgs <- latLonCheck(objArgs, lat, lon, names(data), "update_heatmap")
+  numericCheck(digits)
+
+  allCols <- heatmapColumns()
+  requiredCols <- requiredHeatmapColumns()
+  shape <- createMapObject(data, allCols, objArgs)
+
+  requiredDefaults <- setdiff(requiredCols, names(shape))
+  if(length(requiredDefaults) > 0){
+    shape <- addDefaults(shape, requiredDefaults, "circle")
+  }
+
+  shape <- jsonlite::toJSON(shape, digits = digits)
+
+  invoke_method(map, 'update_heatmap', shape, layer_id)
+}
+
+
+#' @rdname clear
+#' @export
+clear_heatmap <- function(map, layer_id = NULL){
+  layer_id <- layerId(layer_id)
+  invoke_method(map, 'clear_heatmap', layer_id)
+}
