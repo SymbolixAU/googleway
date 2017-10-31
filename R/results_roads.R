5d5ad6c6608cafd26a86ec6f130ee268d77624b8
David Cooley <dcooley@symbolix.com.au>
2017-11-01 07:53:48 +1100

    documentation

--- R/results_roads.R
+++ R/results_roads.R
@@ -1,6 +1,4 @@
 
-#' Nearest Road Coordinates
-#'
 #' @describeIn access_result the coordinates from a nearest roads query
 #' @export
 nearest_roads_coordinates <- function(res) .access_result(resultType(res), "nearest_road_coords")
