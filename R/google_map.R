be1dfc879d4b73c68df4074fc993d6451c4d1245
David Cooley <dcooley@symbolix.com.au>
2018-02-08 12:18:18 +1100

    close #128

--- R/google_map.R
+++ R/google_map.R
@@ -2,23 +2,23 @@
 #'
 #' Generates a google map object
 #'
-#' The data argument is only needed if you call other functions to add layers to the map, such as \code{add_markers()} or \code{add_polylines}. However, the data argument can also be passed into those functions as well.
-#'
-#' In order to use Google Maps you need a valid Google Maps Web JavaScript API key. See the Google Maps API documentation \url{https://developers.google.com/maps/}
-#'
 #' @import htmlwidgets
 #' @import htmltools
 #' @import shiny
 #'
 #' @aliases googleway
 #' @param key A valid Google Maps API key.
-#' @param data data to be used on the map. This will likely contain two columns for latitude and longitude, and / or encoded polylines for plotting polylines and polygons
-#' @param location \code{numeric} vector of latitude/longitude (in that order) coordinates for the initial starting position of the map. The map will automatically set the location and zoom if markers are supplied through \link{add_markers}. If null, the map will default to Melbourne, Australia.
+#' @param data data to be used on the map. Either a data.frame, or an \code{sf} object. See details
+#' @param location \code{numeric} vector of latitude/longitude (in that order)
+#' coordinates for the initial starting position of the map.
+#' The map will automatically set the location and zoom if data is added through one
+#' of the various \link{add_*} functions. If null, the map will default to Melbourne, Australia.
 #' @param zoom \code{integer} representing the zoom level of the map (0 is fully zoomed out)
 #' @param width the width of the map
 #' @param height the height of the map
 #' @param padding the padding of the map
-#' @param styles JSON string representation of a valid Google Maps styles Array. See the Google documentation for details \url{https://developers.google.com/maps/documentation/javascript/styling}
+#' @param styles JSON string representation of a valid Google Maps styles Array.
+#' See the Google documentation for details \url{https://developers.google.com/maps/documentation/javascript/styling}
 #' @param search_box \code{boolean} indicating if a search box should be placed on the map
 #' @param zoom_control logical indicating if the zoom control should be displayed
 #' @param map_type_control logical indicating if the map type control should be displayed
@@ -31,6 +31,27 @@
 #' either an R list, or raw json string.
 #'
 #' @details
+#'
+#' In order to use Google Maps you need a valid Google Maps Web JavaScript API key. See the Google Maps API documentation \url{https://developers.google.com/maps/}
+#'
+#' The data argument is only needed if you call other functions to add layers to the map, such as \code{add_markers()} or \code{add_polylines}. However, the data argument can also be passed into those functions as well.
+#'
+#' The data can either be a data.frame containing longitude and latitude columns
+#' or an encoded polyline for plotting polylines and polygons, or an \code{sf} object.
+#'
+#' The supported \code{sf} object types are
+#'
+#' \itemize{
+#'   \item{POINT}
+#'   \item{MULTIPOINT}
+#'   \item{LINESTRING}
+#'   \item{MULTILINESTRING}
+#'   \item{POLYGON}
+#'   \item{MULTIPOLYGON}
+#'   \item{GEOMETRY}
+#' }
+#'
+#'
 #' The libraries argument can be used to turn-off certain libraries from being called.
 #' By default the map will load
 #' \itemize{
@@ -67,6 +88,7 @@
 #' {"saturation":10},{"lightness":30},{"gamma":0.5},{"hue":"#435158"}]},
 #' {"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"simplified"}]},
 #' {"featureType":"transit.station","elementType":"labels.text","stylers":[{"visibility":"off"}]}]'
+#'
 #' google_map(key = map_key, styles = style)
 #'
 #' }
@@ -228,15 +250,7 @@ update_style <- function(map, styles = NULL){
 #'
 #' server <- function(input, output, session){
 #'
-#' api_key <- "your_api_key"
-#'
-#'   df <- structure(list(lat = c(-37.8201904296875, -37.8197288513184,
-#'   -37.8191299438477, -37.8187675476074, -37.8186187744141, -37.8181076049805
-#'   ), lon = c(144.968612670898, 144.968414306641, 144.968139648438,
-#'   144.967971801758, 144.967864990234, 144.967636108398), weight = c(31.5698964400217,
-#'   97.1629025738221, 58.9051092562731, 76.3215389118996, 37.8982300488278,
-#'   77.1501972114202), opacity = c(0.2, 0.2, 0.2, 0.2, 0.2, 0.2)), .Names = c("lat",
-#'   "lon", "weight", "opacity"), row.names = 379:384, class = "data.frame")
+#'   api_key <- "your_api_key"
 #'
 #'   output$map <- renderGoogle_map({
 #'     google_map(key = api_key)
